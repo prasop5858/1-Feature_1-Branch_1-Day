f07c59011c735389c5f20218cab8c3a6c7f69d92
Prasop Arunprai <arunprai@gmail.com>
2015-05-18 11:50:19 +0700

    Feature frmApplication-Connect Data

--- License/License/frmApplication.vb
+++ License/License/frmApplication.vb
@@ -1,7 +1,13 @@
-﻿Imports License.ClassDefault
+﻿Imports System.Data.OleDb
+Imports License.ClassDefault
 Public Class frmApplication
+    Dim strConn As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\\npfiles\it_develop\prasop\dbLicense.mdb"
+    Dim Conn As New OleDbConnection(strConn)
+    Dim ds As New DataSet
+    Dim da As OleDbDataAdapter
     Private Sub frmApplication_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         ToolStripStatusVersion.Text = strVersion
+        SetDataGridView()
     End Sub
     Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
         Close()
@@ -9,5 +15,17 @@ Public Class frmApplication
     Private Sub BackToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BackToolStripMenuItem.Click
         frmHome.Show()
         Me.Close()
+    End Sub
+    Private Sub SetDataGridView()
+        Dim strSql As String
+        strSql = "SELECT * FROM LICENSE"
+        da = New OleDbDataAdapter(strSql, Conn)
+        da.Fill(ds, "MyData")
+        Me.DataGridViewApplication.DataSource = ds.Tables("MyData")
+    End Sub
+    Private Sub RefreshData()
+        If ds.Tables.Contains("MyData") Then
+            ds.Tables.Remove("MyData")
+        End If
     End Sub
 End Class
